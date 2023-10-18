2abf8ef43d492df64e76bf2d36223f3b4c0cc0b8
imminpie <mjkang0715@gmail.com>
2023-10-18 16:23:27 +0900

    feat: 블로그 글 등록 기능 구현하기

--- src/pages/PostNew.jsx
+++ src/pages/PostNew.jsx
@@ -1,5 +1,76 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
 
 export default function PostNew() {
-  return <>블로그 글 등록</>;
+  const [post, setPost] = useState({ id: '', title: '', content: '' });
+  const [store, setStore] = useState(() => readTodoFromLocalStorage());
+  const navigate = useNavigate();
+
+  const titleInput = useRef();
+  const contentInput = useRef();
+
+  const handleChange = (e) => {
+    setPost((prev) => ({
+      ...prev,
+      id: uuidv4(),
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!post.title.trim()) {
+      titleInput.current.focus();
+      return alert('제목을 입력하세요.');
+    }
+
+    if (!post.content.trim()) {
+      contentInput.current.focus();
+      return alert('내용을 입력하세요.');
+    }
+
+    setStore((prev) => [...prev, post]);
+    setPost({ id: '', title: '', content: '' });
+  };
+
+  useEffect(() => {
+    localStorage.setItem('posts', JSON.stringify(store));
+  }, [store]);
+
+  return (
+    <form className='form' onSubmit={handleSubmit}>
+      <input
+        type='text'
+        className='input'
+        ref={titleInput}
+        name='title'
+        value={post.title}
+        onChange={handleChange}
+        placeholder='제목을 입력하세요'
+      />
+      <textarea
+        className='textarea'
+        ref={contentInput}
+        name='content'
+        value={post.content}
+        onChange={handleChange}
+        placeholder='내용을 입력하세요'
+      ></textarea>
+      <div className='buttons'>
+        <button type='submit' className='btn'>
+          등록
+        </button>
+        <button type='button' className='btn' onClick={() => navigate('/')}>
+          취소
+        </button>
+      </div>
+    </form>
+  );
+}
+
+function readTodoFromLocalStorage() {
+  const posts = localStorage.getItem('posts');
+  return posts ? JSON.parse(posts) : [];
 }
