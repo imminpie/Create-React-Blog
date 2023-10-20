abc4e5a692e02d3576baeab5c3f4f03284b9655f
imminpie <mjkang0715@gmail.com>
2023-10-20 23:50:36 +0900

    feat: 새 글 작성 버튼 새로고침 기능 구현

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
+import styles from '../css/Navbar.module.css';
 
 export default function Navbar() {
+  const { pathname } = useLocation();
+  const handleClick = () => {
+    if (pathname === '/post/new') {
+      window.location.reload();
+    }
+  };
   return (
-    <header>
-      <Link to='/'>
-        <h1>블로그</h1>
-      </Link>
-      <Link to='/post/new'>새 글 작성</Link>
+    <header className={`${styles.header} wrap`}>
+      <nav className={`${styles.nav} inner`}>
+        <Link to='/'>
+          <h1 className={styles.logo}>BLOG</h1>
+        </Link>
+        <Link to='/post/new' className={styles.new} onClick={handleClick}>
+          새 글 작성
+        </Link>
+      </nav>
     </header>
   );
 }
